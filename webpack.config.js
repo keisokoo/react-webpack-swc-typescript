a7e26f206da22ae29b6b8aedde3083185c5320c2
keisokoo <keisokoo@doubled.kr>
2022-07-29 13:04:17 +0900

    ,

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,6 @@ module.exports = (currentEnv) => {
       isDevelopment && require.resolve('style-loader'),
       !isDevelopment && {
         loader: MiniCssExtractPlugin.loader,
-        // css is located in `static/css`, use '../../' to locate index.html folder
-        // in production `paths.publicUrlOrPath` can be a relative path
         options: {},
       },
       {
@@ -28,14 +26,9 @@ module.exports = (currentEnv) => {
         options: cssOptions,
       },
       {
-        // Options for PostCSS as we reference these options twice
-        // Adds vendor prefixing based on your specified browser support in
-        // package.json
         loader: require.resolve('postcss-loader'),
         options: {
           postcssOptions: {
-            // Necessary for external CSS imports to work
-            // https://github.com/facebook/create-react-app/issues/2677
             ident: 'postcss',
             config: false,
             plugins: [
@@ -49,9 +42,6 @@ module.exports = (currentEnv) => {
                   stage: 3,
                 },
               ],
-              // Adds PostCSS Normalize as the reset css with default options,
-              // so that it honors browserslist config in package.json
-              // which in turn let's users customize the target behavior as per their needs.
               'postcss-normalize',
             ],
           },
