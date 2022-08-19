03b0dcdc21c69ead3ae7f86577e7b9bdea9d6eeb
keisokoo <keisokoo@doubled.kr>
2022-08-19 10:53:45 +0900

    fix, tokens

--- src/layout/AuthGuard.tsx
+++ src/layout/AuthGuard.tsx
@@ -3,12 +3,14 @@ import useAuth, {
   isToken,
   TokenType,
 } from '@/lib/coils/auth'
+import dayjs from 'dayjs'
 import { useEffect } from 'react'
 import Loading from './Loading'
 
 interface AuthGuardProps {
   children?: React.ReactNode
 }
+console.log('dayjs', dayjs().add(3, 'minute').toISOString())
 const AuthGuard = ({ children }: AuthGuardProps) => {
   const { authStatus, setToken } = useAuth()
   useEffect(() => {
@@ -41,12 +43,10 @@ const AuthGuard = ({ children }: AuthGuardProps) => {
           <button
             onClick={async () => {
               const testToken = {
-                accessToken:
-                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJsIjowLCJpYXQiOjE2NTg3OTY4NjcsImV4cCI6MTY1ODc5NzQ2N30.n0J56HnDAZfhIZgQA8NnqL9psMDaVgjh-F2f5P8Loro',
-                accessTokenExpiresIn: '2022-07-26 10:04:27.12400 GMT+9',
-                refreshToken:
-                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJsIjowLCJoIjoiJDJiJDEwJHJEOWRlV2cubTAud1ZUOWZ5eERQdi5BcFV5TDZDTFgzS0t1b1dJdVdNQlFNUGs3THpIdVZTIiwiaWF0IjoxNjU4Nzk2ODY3LCJleHAiOjE2NjEzODg4Njd9.1dTmZx6CHhX2yD-YECX7pZKk7wwnl5DT5GKcSIcgYtM',
-                refreshTokenExpiresIn: '2022-08-25 09:54:27.12400 GMT+9',
+                accessToken: 'access',
+                accessTokenExpiresIn: dayjs().add(3, 'minute').toISOString(),
+                refreshToken: 'refreshToken',
+                refreshTokenExpiresIn: dayjs().add(1, 'year').toISOString(),
               }
               setToken(await handleTokenToLocalStorage(testToken))
             }}
